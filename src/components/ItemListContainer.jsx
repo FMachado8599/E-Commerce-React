17680a9e3956462a5e64c9b75c0b80e9445d95d8
Facundo Machado <f.machado5899@gmail.com>
2024-05-16 22:19:41 -0300

    Agregada imagen con componente BS 'Imagen'

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image';
+import ImagenProducto from "../multimedia/pc_zeus1.webp"
 
 export const ItemListContainer = () => {
   return (
@@ -6,6 +8,7 @@ export const ItemListContainer = () => {
     <div className="productos">
       <h1>Esto es un e-commerce de productos</h1>
       <h2>Estoy tratando de tener paciencia con REACT</h2>
+          <Image src={ImagenProducto} thumbnail />
     </div>
   )
 }
